cfdbc71aa3ee36e6d7ffbcaee77a7913c2c63132
kamichidu <c.kamunagi@gmail.com>
2017-09-09 08:33:17 +0900

    Use gofmt for code formatting

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"go/format"
 	"io"
 	"log"
 	"os"
@@ -20,12 +22,14 @@ func run(in io.Reader, out io.Writer, errOut io.Writer, args []string) int {
 		pkgName     string
 		typName     string
 		ofilename   string
+		noGofmtFlag bool
 	)
 	flg := flag.NewFlagSet(filepath.Base(args[0]), flag.ExitOnError)
 	flg.BoolVar(&versionFlag, "v", false, "Show version")
 	flg.StringVar(&pkgName, "p", "main", "Package name")
 	flg.StringVar(&typName, "t", "", "Publisher type name")
 	flg.StringVar(&ofilename, "o", "-", "Output filename")
+	flg.BoolVar(&noGofmtFlag, "no-gofmt", false, "Don't use gofmt for generated go code")
 	if err := flg.Parse(args[1:]); err != nil {
 		log.Printf("Argument error: %s", err)
 		return 128
@@ -34,6 +38,12 @@ func run(in io.Reader, out io.Writer, errOut io.Writer, args []string) int {
 	if versionFlag {
 		fmt.Println(appVersion)
 		return 0
+	}
+
+	buffer := new(bytes.Buffer)
+	if err := generate(buffer, pkgName, typName, flg.Args()); err != nil {
+		log.Printf("Can't generate code: %s", err)
+		return 1
 	}
 
 	var w io.Writer
@@ -49,8 +59,18 @@ func run(in io.Reader, out io.Writer, errOut io.Writer, args []string) int {
 		}
 	}
 
-	if err := generate(w, pkgName, typName, flg.Args()); err != nil {
-		log.Printf("Can't generate code: %s", err)
+	var generatedContent []byte
+	if !noGofmtFlag {
+		var err error
+		generatedContent, err = format.Source(buffer.Bytes())
+		if err != nil {
+			log.Panic("Can't gofmt output: %s", err)
+		}
+	} else {
+		generatedContent = buffer.Bytes()
+	}
+	if _, err := w.Write(generatedContent); err != nil {
+		log.Printf("Can't write output: %s", err)
 		return 1
 	}
 
